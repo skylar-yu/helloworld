29bbe6bdd34de6d312683704a11dc5216b336684
skylar-yu <745369408@qq.com>
2017-05-26 11:57:23 +0800

    Initial commit

---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+public class TestMain {
+
+    public static void main(String [] args){
+        System.out.println("hwlllworllldd...");
+    }
+}
---
+++ src/com/oneplus/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.oneplus.test;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+public class HelloWorld {
+
+        main
+}
